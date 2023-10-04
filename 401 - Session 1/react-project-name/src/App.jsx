9734087a84053796c7e3a3d0ed175a0a3e07a277
purplebearddan <daniel_m@purplebeard.co.uk>
2023-10-04 17:57:34 +0100

    added comments to the first session

--- 401 - Session 1/react-project-name/src/App.jsx
+++ 401 - Session 1/react-project-name/src/App.jsx
@@ -1,28 +1,27 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 // import './App.css'
 
-
 function App() {
-  const [data, setData] = useState(null)
-
+  const [data, setData] = useState(null);
 
   // when the page loads
   useEffect(() => {
-
     // fetch some data
-    fetch('https://api.github.com/users/mowglixx/repos')
-      .then(data => data.json())
-      // set the state of the variable "data" with the json from the request using the useState hook
-      .then(json => setData(json))
-  })
+    fetch("https://api.github.com/users/mowglixx/repos")
+      // convert the data to a usable JS Object
+      .then((data) => data.json())
+      // set the state of the variable "data" with the json from the
+      // request using the setData function from the useState hook
+      .then((json) => setData(json));
+  });
 
   return (
     <>
       <ul>
-
         {data && data.map((repo) => <li key={repo.id}>{repo.full_name}</li>)}
-      </ul>    </>
-  )
+      </ul>{" "}
+    </>
+  );
 }
 
-export default App
+export default App;
