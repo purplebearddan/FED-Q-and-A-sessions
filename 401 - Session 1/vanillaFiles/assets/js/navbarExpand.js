b79c8ff847d90887121dba68f1f371a1a4421603
purplebearddan <daniel_m@purplebeard.co.uk>
2023-10-04 20:44:20 +0100

    lesson complete

--- 401 - Session 1/vanillaFiles/assets/js/navbarExpand.js
+++ 401 - Session 1/vanillaFiles/assets/js/navbarExpand.js
@@ -1,29 +1,23 @@
-
 // Select the button that will expand the navigation
-const navExpandButton = document.querySelector('#nav-expand-button')
+const navExpandButton = document.querySelector("#nav-expand-button");
 
 // select the element that is your navigation
-const nav = document.getElementById('mainNav')
+const nav = document.getElementById("mainNav");
 
-
-// create a function to tell the 
+// create a function to tell the
 // browser what to do when the button is clicked
 function handleClick() {
-    
-    // toggle a class of 'open' on the nav element, this will 
-    // change the visual style to `display: flex;` from `display: none;`
-    nav.classList.toggle('open')
-    
+  // toggle a class of 'open' on the nav element, this will
+  // change the visual style to `display: flex;` from `display: none;`
+  nav.classList.toggle("open");
 
-    // get the state of the 'aria-expanded' attribute
-    const expandedState = nav.getAttribute('aria-expanded')
-        
-    // set the attribute of 'aria-expanded' to the opposite of what it currently is
-    nav.setAttribute('aria-expanded', !eval(expandedState))
+  // get the state of the 'aria-expanded' attribute
+  const expandedState = nav.getAttribute("aria-expanded");
 
-};
+  // set the attribute of 'aria-expanded' to the opposite of what it currently is
+  nav.setAttribute("aria-expanded", !eval(expandedState));
+}
 
-
-// tell the browser to listen for a click event 
+// tell the browser to listen for a click event
 // and to run "handleClick" when it is clicked
-navExpandButton.addEventListener('click', handleClick)
+navExpandButton.addEventListener("click", handleClick);
--- 401 - Session 3/README.md
+++ 401 - Session 3/README.md
@@ -2,7 +2,13 @@
 
 ## Topics
 
-- 
+- Classes X
+    - `this` keyword in JS Classes (Edil & Ili (on behalf of Artus), Hassan) X
+    - Constructors (Jack) X
+    - Super (Jack)
+- Promises (in depth) (Ili on behalf of self) X
+- API's (Isaac)
+- Tom was doing [this Kata on codewars](https://www.codewars.com/kata/585894545a8a07255e0002f1/train/javascript)
 
 ---
 
